ce9191fedae376338443401324880b4a4c8b8749
Michael Craft <mcraft@peak15.org>
2013-03-16 17:52:53 -0400

    Rule Tests are ready, needs example file trees. Need to refactor ConvertCollection to reusable object. Need to implement WorkSet test. Need to implement CollectionConverter test.

--- src/org/peak15/convertcollection/test/RuleTests.java
+++ src/org/peak15/convertcollection/test/RuleTests.java
@@ -3,8 +3,11 @@ package org.peak15.convertcollection.test;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 import org.junit.Test;
 import org.peak15.convertcollection.FatalConversionException;
@@ -23,12 +26,32 @@ import org.peak15.convertcollection.rules.Rule;
  */
 public class RuleTests extends ValueTypeTest<Rule> {
 	
-	private static final String DESTINATION_DIR = "";
 	private static final File SOURCE_DIR = new File("");
+	private static final String DESTINATION_DIR = "";
+	private static final String DESTINATION_DIR_2 = "";
+	
+	private static final Queue<File> SOURCE_FILES = new LinkedList<File>(Arrays.<File>asList(
+			null,
+			null));
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDirectory() {
+		assertEquals(SOURCE_DIR, a.directory());
+	}
+	
+	@Test
+	public void testProcedure() {
+		assertNotNull(a.procedure());
+	}
+	
+	@Test
+	public void testTraversalRule() {
+		try {
+			assertEquals(SOURCE_FILES, a.traversalRule().filesOf(SOURCE_DIR));
+		} catch (FatalConversionException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
 	}
 
 	@Override
@@ -48,8 +71,17 @@ public class RuleTests extends ValueTypeTest<Rule> {
 
 	@Override
 	protected Rule D() {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> args = Collections.singletonList(DESTINATION_DIR_2);
+		
+		Rule ret = null;
+		try {
+			ret = MP3ify.builder().build(SOURCE_DIR, args);
+		} catch (FatalConversionException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		return ret;
 	}
 
 }
