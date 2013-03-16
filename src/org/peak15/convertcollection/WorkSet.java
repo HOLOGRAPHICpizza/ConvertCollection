e308a75c0ea7af6be0bc065c3f0fbbccf39dd582
Michael Craft <mcraft@peak15.org>
2013-03-16 00:05:32 -0400

    worked on work set

--- src/org/peak15/convertcollection/WorkSet.java
+++ src/org/peak15/convertcollection/WorkSet.java
@@ -10,11 +10,18 @@ import java.util.Set;
  * Fill it up then run it.
  * The work will be performed on multiple threads.
  * 
+ * Not thread safe.
+ * 
  * @param T the type of the problems to solve (type of work set elements)
  */
 public final class WorkSet<T> {
 	
 	private Set<T> workPool = Collections.<T>synchronizedSet(new HashSet<T>());
+	
+	private final Object syncLock = new Object();
+	
+	private static final int THREADS = Runtime.getRuntime().availableProcessors();
+	private int currentThreads = 0;
 	
 	public WorkSet<T> add(T item) {
 		if(item == null) {
@@ -41,6 +48,57 @@ public final class WorkSet<T> {
 	 * After this, the set is empty and ready for more work.
 	 */
 	public void doWork(Procedure<T> procedure) {
-		throw new UnsupportedOperationException("Not yet implemented!");
+		//throw new UnsupportedOperationException("Not yet implemented!");
+		
+		if(procedure == null) {
+			throw new NullPointerException("Procedure may not be null.");
+		}
+		
+		// spawn threads to pull from the work pool
+		// get a lock so the workers can't prematurely notify
+		synchronized(this.syncLock) {
+			for(currentThreads=0; currentThreads < THREADS; currentThreads++) {
+				new Thread(new Worker<T>(this.workPool)).start();
+			}
+			
+			// Wait on our workers.
+			// The lock will be notified when all workers are done.
+			try {
+				this.syncLock.wait();
+			} 
+			catch(InterruptedException e) {}
+		}
+	}
+	
+	/**
+	 * Children call this when they have no more work.
+	 * Children MUST DIE after this call!
+	 */
+	private void signalWorkDone() {
+		synchronized(this.syncLock) {
+			currentThreads--;
+			
+			if(currentThreads < 0) {
+				// All work is done, notify the lock.
+				currentThreads = 0;
+				this.syncLock.notify();
+			}
+		}
+	}
+	
+	private static final class Worker<T> implements Runnable {
+		
+		private final Set<T> workPool;
+		
+		private Worker(Set<T> workPool) {
+			this.workPool = workPool;
+		}
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			throw new UnsupportedOperationException("Not yet implemented!");
+		}
+		
 	}
 }
